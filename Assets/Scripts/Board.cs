e48233c1589a2943cf46e123c4a13766deb716b9
Starttera <efrknsngl@gmail.com>
2024-08-08 20:41:28 +0300

    We added win or draw mechanics

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -22,11 +22,21 @@ public class Board : MonoBehaviour
     }
   }
 
+  public void Reset()
+  {
+    foreach (Cell cell in _cells )
+    {
+      cell.label.text = "";
+      cell.btn.interactable = true;
+    }
+  }
+
   public bool CheckForWinner()
   {
     int i = 0;
     for(i = 0; i<=6;i+=3)
     {
+      // Horizontal
       if (!CheckValues(i,i+1))
       {
         continue;
@@ -38,7 +48,7 @@ public class Board : MonoBehaviour
       }
       return true;
     }
-
+    // Vertical
     for (i = 0; i <= 2; i++)
     {
       if (!CheckValues(i,i+3))
@@ -53,6 +63,20 @@ public class Board : MonoBehaviour
 
       return true;
     }
+    // left Diagonal
+    if (CheckValues(0,4)&& CheckValues(0,8))
+      // yani 0 ilk kare oluyor 4 tane ileri gidersek tam ortadaki çapraz oluyor
+    // 8 tane sağ en üst capraz oluyor
+    {
+      return true;
+    }
+    // right diagonal
+    if (CheckValues(2,4)&& CheckValues(2,8))
+      // 2 yapma sebebim sağdan kontrol edicez
+    {
+      return true;
+    }
+    
     
     return false;
   }
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -2,29 +2,34 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Main : MonoBehaviour
 {
     public Board board;  
     private bool xTurn = true;
     private int xTurnCount = 0; // kaç kere geldiğini hesaplamak için
-    
+    public GameObject Winner;
     private void Awake()
     {
      board.Build(this);
      // Parametre aldığı için bu sınıfın tipinden burada cağırıyoruz.
     }
+    
 
     public void Switch() // harfleri değiştiren metod
     {
         xTurnCount++; // her sefer bir artmasını istiyorum.
-        xTurn = !xTurn;  // Ünlem işareti onu tam tersine çevirir.
-
+        
         bool hasWinner = board.CheckForWinner();
-        if (hasWinner)
+        if (hasWinner || xTurnCount == 9)
         {
-            Debug.Log("Winneer!!!");
+            // End Game
+            StartCoroutine(EndGame(hasWinner));
+            return;
         }
+        xTurn = !xTurn;  // Ünlem işareti onu tam tersine çevirir.
+        
     }
 
     public string GetTurnCharacter()
@@ -38,4 +43,30 @@ public class Main : MonoBehaviour
             return " O ";
         }
     }
+
+    private IEnumerator EndGame(bool hasWinner)
+    {
+        Text winnerLabel = Winner.GetComponentInChildren<Text>();
+        
+
+        if (hasWinner)
+        {
+            winnerLabel.text = GetTurnCharacter() + " " + "Kazannn...";
+        }
+        else
+        {
+            winnerLabel.text = "Berabere";
+        }
+
+        Winner.SetActive(true);
+        WaitForSeconds wait = new WaitForSeconds(5.0f);
+        yield return wait;
+        
+        board.Reset();
+        xTurnCount = 0;
+        Winner.SetActive(false);
+        // Sıfırlama işlemi
+        
+    }
+    
 }
